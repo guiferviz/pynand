1ec35cfd4a8f747ec75033fdb3accd9cb1d425e6
guiferviz <guiferviz@gmail.com>
2023-01-09 11:32:25 +0100

    update template: moving module to src dir

--- tests/test_wire.py
+++ tests/test_wire.py
@@ -1,21 +1,7 @@
 """Tests for `wire` package."""
 
-import pytest
 
-from wire import wire
+def test_import():
+    import wire as wire
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/johanvergeer/cookiecutter-poetry')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert wire
